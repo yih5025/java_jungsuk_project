f78e7c422c0a7e692a6112717db4d9ec1f1533f2
yih5025 <yih5025@naver.com>
2023-06-13 23:35:48 +0900

    overflow

---
+++ OverflowEx.java
@@ -0,0 +1,10 @@
+public class OverflowEx {
+    public static void main(String[] args) {
+        short sMin = -32768;
+        short sMax = 32767;
+        char cMin = 0;
+        char cMax = 65535;
+
+        System.out.
+    }
+}
